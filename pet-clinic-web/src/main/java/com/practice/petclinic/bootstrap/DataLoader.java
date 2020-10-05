c62b5bb547b325b5e2d4a34c8451266695250133
David Temple <david.temple@kcl.ac.uk>
2020-10-05 11:02:28 +0100

    Add Auto inc id. closes #22

--- pet-clinic-web/src/main/java/com/practice/petclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/practice/petclinic/bootstrap/DataLoader.java
@@ -21,14 +21,12 @@ public class DataLoader implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         Owner owner1 = new Owner();
-        owner1.setId(1L);
         owner1.setFirstName("Mike");
         owner1.setLastName("West");
 
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
-        owner2.setId(2L);
         owner2.setFirstName("Dave");
         owner2.setLastName("Tee");
 
@@ -37,14 +35,12 @@ public class DataLoader implements CommandLineRunner {
         System.out.println("Loaded Owners...");
 
         Vet vet1 = new Vet();
-        vet1.setId(1L);
         vet1.setFirstName("Connor");
         vet1.setLastName("McG");
 
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
-        vet2.setId(2L);
         vet2.setFirstName("Alice");
         vet2.setLastName("Porter");
 
