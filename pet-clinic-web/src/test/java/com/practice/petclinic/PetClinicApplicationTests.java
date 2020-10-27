50a3bf1e01efa0b9bba99310a2fccb565b2a3e99
David Temple <david.temple@kcl.ac.uk>
2020-10-27 19:29:02 +0000

    Project already using JUnit5 comments added. Closes #45

--- pet-clinic-web/src/test/java/com/practice/petclinic/PetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/com/practice/petclinic/PetClinicApplicationTests.java
@@ -3,6 +3,7 @@ package com.practice.petclinic;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+// using junit 5
 @SpringBootTest
 class PetClinicApplicationTests {
 
